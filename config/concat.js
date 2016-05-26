b3048b4d635e21da9ee12b754e7bac4bd989cb2a
edison <edison@rtaylor.mshanken.com>
2016-05-26 16:44:19 -0400

    jquery was not being concatenating

--- config/concat.js
+++ config/concat.js
@@ -3,6 +3,7 @@
 module.exports = {
   js: {
     src: [
+      '<%= config.prod.script %>/vendor/jquery-1.11.1.js',
       '<%= config.prod.script %>/vendor/*.js',
       '<%= config.prod.script %>/app.js',
       '!<%= config.prod.script %>/vendor/modernizr.js',
