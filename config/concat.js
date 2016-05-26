872cb674db55ec944276f2d1a1825df73353e9aa
edison <edison@rtaylor.mshanken.com>
2016-05-26 14:47:37 -0400

    collabse menu for mobile: inserted

--- config/concat.js
+++ config/concat.js
@@ -3,7 +3,6 @@
 module.exports = {
   js: {
     src: [
-      '<%= config.prod.script %>/vendor/jquery*.js',
       '<%= config.prod.script %>/vendor/*.js',
       '<%= config.prod.script %>/app.js',
       '!<%= config.prod.script %>/vendor/modernizr.js',
