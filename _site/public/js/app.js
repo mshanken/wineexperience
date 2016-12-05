51a45e437dc79ff20001f9214f72aa67a0db8046
Edison Leon <eleon@mshanken.com>
2016-12-05 15:55:23 -0500

    turning off event but welcome next one

--- _site/public/js/app.js
+++ _site/public/js/app.js
@@ -1,5 +1,16 @@
 (function () {
   "use strict";
+  window.sr = new ScrollReveal();
 
-  // console.log("loaded...");
+  var fooReveal = {
+	delay    : 200,
+	distance : '90px',
+	easing   : 'ease-in-out',
+	rotate   : { z: 10 },
+	scale    : 1.1
+  };
+
+  sr.reveal('.evenends', fooReveal);
+  sr.reveal('.evenends blockquote', fooReveal);
+
 })($);
